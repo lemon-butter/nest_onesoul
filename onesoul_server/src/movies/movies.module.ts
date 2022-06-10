999ec0f6a4a1b4d50e81e5fbe746b24964cf97c8
Junginet <kim1231401@naver.com>
2022-06-10 21:38:21 +0900

    second

--- onesoul_server/src/movies/movies.module.ts
+++ onesoul_server/src/movies/movies.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionOptions } from 'typeorm';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
 
 @Module({
+  imports: [
+    MoviesModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+        }),
+      })
+  ],
   controllers: [MoviesController],
   providers: [MoviesService],
 })
